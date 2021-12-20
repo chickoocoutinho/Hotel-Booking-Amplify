8d94e8b7a16bba3c836d373634351923735756c0
chickoocoutinho <benito.coutinho@gmail.com>
2021-12-20 10:21:59 +0530

    added loader key

--- src/App.js
+++ src/App.js
@@ -1,38 +1,49 @@
-import React,{useRef} from 'react';
+import React, { useRef } from "react";
+import { Routes, Route, Link } from "react-router-dom";
 
-import Navbar from './components/Navbar/Navbar';
-import HeroImage from './components/HeroImage/HeroImage';
-import CheckInForm from './components/CheckInForm/CheckInForm';
-import About from './components/About/About';
-import Rooms from './components/Rooms/Rooms';
-import Services from './components/Services/Services';
-import ThingsToDo from './components/Services/ThingsToDo';
-import Reviews from './components/Rooms/Reviews';
-import ContactUs from './components/ContactUs/ContactUs';
-import Footer from './components/Footer/Footer';
-import Gallery from './components/Gallery/Gallery';
-import FloatingButton from './components/FloatingButton/FloatingButton';
+import Navbar from "./components/Navbar/Navbar";
+import HeroImage from "./components/HeroImage/HeroImage";
+import CheckInForm from "./components/CheckInForm/CheckInForm";
+import About from "./components/About/About";
+import Rooms from "./components/Rooms/Rooms";
+import Services from "./components/Services/Services";
+import ThingsToDo from "./components/Services/ThingsToDo";
+import Reviews from "./components/Rooms/Reviews";
+import ContactUs from "./components/ContactUs/ContactUs";
+import Footer from "./components/Footer/Footer";
+import Gallery from "./components/Gallery/Gallery";
+import FloatingButton from "./components/FloatingButton/FloatingButton";
 
 const App = () => {
-
-  const divRef= useRef(null);
+  const divRef = useRef(null);
 
   return (
-    <>
-      <Navbar />
-      <HeroImage />
-      <CheckInForm />
-      <About divRef={divRef}/>
-      <Rooms />
-      <Services />
-      <Gallery />
-      <ThingsToDo />
-      <Reviews /> 
-      <ContactUs />
-      <Footer />
-      <FloatingButton divRef={divRef}/>
-    </>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <Navbar />
+            <HeroImage />
+            <CheckInForm />
+            <About divRef={divRef} />
+            <Rooms />
+            <Services />
+            <Gallery />
+            <ThingsToDo />
+            <Reviews />
+            <ContactUs />
+            <Footer />
+            <FloatingButton divRef={divRef} />
+          </>
+        }
+      />
+      <Route
+        path="/loaderio-5007c461bb06931e72fd35106832d91e"
+        element={<p>loaderio-5007c461bb06931e72fd35106832d91e</p>}
+      />
+    </Routes>
   );
-}
+};
 
 export default App;
